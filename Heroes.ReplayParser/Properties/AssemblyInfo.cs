45cdc7720b9a55bebb659e73b79c0ff703f8d781
Kevin Oliva <kevin.k.oliva@gmail.com>
2016-11-28 17:53:26 -0600

    Changed the versioning number

--- Heroes.ReplayParser/Properties/AssemblyInfo.cs
+++ Heroes.ReplayParser/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Heroes.ReplayParser")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ab85402d-b34a-42ed-b74e-02c2656896d8")]
-[assembly: AssemblyVersion("1.3.0")]
-[assembly: AssemblyFileVersion("1.3.0")]
+[assembly: AssemblyVersion("1.0.0.48297")]
+[assembly: AssemblyFileVersion("1.0.0.48297")]
