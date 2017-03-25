0ba36134a16591c0155b8ace4f203c94b7c7e15d
Kevin Oliva <kevin.k.oliva@gmail.com>
2017-03-25 11:24:01 -0500

    Updated assembly version

--- Heroes.ReplayParser/Properties/AssemblyInfo.cs
+++ Heroes.ReplayParser/Properties/AssemblyInfo.cs
@@ -7,5 +7,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Heroes.ReplayParser")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ab85402d-b34a-42ed-b74e-02c2656896d8")]
-[assembly: AssemblyVersion("1.0.0.51779")]
-[assembly: AssemblyFileVersion("1.0.0.51779")]
+[assembly: AssemblyVersion("2.0.0.51779")]
+[assembly: AssemblyFileVersion("2.0.0.51779")]
