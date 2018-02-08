7a4b95d0d6058446ef0888cb9972ecd1fab60ab3
Kevin Oliva <kevin.k.oliva@gmail.com>
2018-02-07 18:47:07 -0600

    Updated latest support build

--- Heroes.ReplayParser/Replay.cs
+++ Heroes.ReplayParser/Replay.cs
@@ -8,7 +8,7 @@
         /// <summary>
         /// Latest build supported by the parser
         /// </summary>
-        public static int LatestSupportedBuild => 59657;
+        public static int LatestSupportedBuild => 61952;
 
         /// <summary> Gets a list of all messages which took place during the game. </summary>
         public List<Message> Messages { get; set; } = new List<Message>();
